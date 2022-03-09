126417632423e110e4301c4c78cc3d46e5c0a400
Nugroggo <62874999+deathbycakes@users.noreply.github.com>
2022-03-09 10:38:08 +0700

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ var APP_DATA = {
   "scenes": [
     {
       "id": "0-a_interactive-lightmix",
-      "name": "a_Interactive LightMix",
+      "name": "kmr",
       "levels": [
         {
           "tileSize": 256,
@@ -27,46 +27,10 @@ var APP_DATA = {
       "linkHotspots": [],
       "infoHotspots": [
         {
-          "yaw": 0.7227782400768881,
-          "pitch": 0.417383763417261,
-          "title": "Meja",
-          "text": "KULLABERG IKEA\n<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br></p>\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Lebar:</p>\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">110 cm</p>\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Kedalaman:</p>\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">70 cm</p>\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Tinggi:</p>\n<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">75 cm</p>\n<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br><br></p>"
-        },
-        {
-          "yaw": 1.4598350339596857,
-          "pitch": -0.20799533749532273,
-          "title": "Ambalan",
-          "text": "L 80 kali<br>"
-        },
-        {
-          "yaw": -0.16677577831302592,
-          "pitch": 0.09996287416602101,
-          "title": "rak buku<br>",
-          "text": "60x25 ? cari yg ramping <br>"
-        },
-        {
-          "yaw": -1.425651849298017,
-          "pitch": 0.4260741048298513,
-          "title": "drawer",
-          "text": "kabinet, 4 drawer L 50-70<br>"
-        },
-        {
-          "yaw": 2.1586407990239582,
-          "pitch": 1.0503102187907505,
-          "title": "bed",
-          "text": "<div>inthebox 100x200</div><div><br></div>"
-        },
-        {
-          "yaw": -0.5048430057707574,
-          "pitch": 1.0769518734432015,
-          "title": "rugs",
-          "text": "nah mending pake vinyl aj<br>"
-        },
-        {
           "yaw": -0.94015437980417,
           "pitch": -0.4755295559039556,
           "title": "ruangan",
-          "text": "<div>2,2 x 3 m <br></div><div>--</div><div>cat stell blue atau ungu lavender <br></div><div>-- <br></div><div><br></div>"
+          "text": "<div>2,2 x 3 m <br></div><div>--</div><div> <br></div><div>-- <br></div><div><br></div>"
         }
       ]
     }
